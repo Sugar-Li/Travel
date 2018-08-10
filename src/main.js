3a1fe03cfd20cbc02ce24c561fc3c15d2324a649
Sugar_glb <jzsq_lyx-glb@sina.com>
2018-08-10 12:22:24 +0800

    init the program-Travel

---
+++ src/main.js
@@ -0,0 +1,19 @@
+
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import './assets/css/reset.css'
+import './assets/css/border.css'
+import fastClick from 'fastclick'
+
+Vue.config.productionTip = false
+
+fastClick.attach(document.body)
+
+
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
